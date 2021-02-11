87af9270a74ef386d12c52be8917e8b4618916e3
PaperMartin <martin.wetischek@orange.fr>
2021-02-11 09:26:02 +0100

    card selector

---
+++ Assets/Scripts/AR/CardSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CardSelector : MonoBehaviour
+{
+    [System.Serializable]
+    public struct CardID
+    {
+        public string id;
+        public GameObject gameObject;
+    }
+
+    [SerializeField]
+    private List<CardID> cards = new List<CardID>();
+
+    private Dictionary<string,GameObject> cardDictionary = new Dictionary<string, GameObject>();
+    private ARTrackedImage image;
+
+    private void Start() {
+        foreach(CardID id in cards){
+            cardDictionary.Add(id.id,id.gameObject);
+        }
+        image = GetComponent<ARTrackedImage>();
+        cardDictionary[image.referenceImage.name].gameObject.SetActive(true);
+    }
+}
