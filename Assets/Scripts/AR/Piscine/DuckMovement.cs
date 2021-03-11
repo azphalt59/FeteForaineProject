d9c29e21e3208a769b44f0b4cf2589f34a980335
PaperMartin <martin.wetischek@orange.fr>
2021-03-11 08:54:48 +0100

    water shader

---
+++ Assets/Scripts/AR/Piscine/DuckMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float StartOffset;
+    [SerializeField]
+    private float RotationTime;
+    [SerializeField]
+    private float DistanceFromCenter;
+    [SerializeField]
+    private float DistanceNoiseStrength;
+    [SerializeField]
+    private float DistanceNoiseScale;
+
+    [SerializeField]
+    private Transform CentrePiscine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = GetPosition((Time.time + StartOffset) * RotationTime * 360);
+    }
+
+    Vector3 GetPosition(float Time){
+        Vector3 pos = new Vector3(0,CentrePiscine.position.y,DistanceFromCenter);
+        
+        float distanceNoise = Mathf.PerlinNoise(Time * DistanceNoiseScale,0);
+        distanceNoise = distanceNoise - 0.5f;
+        distanceNoise = distanceNoise * DistanceNoiseStrength;
+        pos.z += distanceNoise;
+
+        Vector3 finalpos = RotatePointAroundPivot(pos, CentrePiscine.position, new Vector3(0,Time,0));
+
+        return finalpos;
+    }
+
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
+    return Quaternion.Euler(angles) * (point - pivot) + pivot;
+  }
+}
