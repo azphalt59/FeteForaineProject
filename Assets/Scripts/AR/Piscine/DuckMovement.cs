609ead8deec7b22abb3470d31bb108b3dbf00753
PaperMartin <martin.wetischek@orange.fr>
2021-03-18 09:23:36 +0100

    canne a pèche model

--- Assets/Scripts/AR/Piscine/DuckMovement.cs
+++ Assets/Scripts/AR/Piscine/DuckMovement.cs
@@ -14,9 +14,17 @@ public class DuckMovement : MonoBehaviour
     private float DistanceNoiseStrength;
     [SerializeField]
     private float DistanceNoiseScale;
+    [SerializeField]
+    private float BobblingSpeed = 2;
+    [SerializeField]
+    private float BobbleAngle = 15;
+    [SerializeField]
+    private float FloatFactor = 0.0625f / 2;
 
     [SerializeField]
     private Transform CentrePiscine;
+    [SerializeField]
+    private Transform Model;
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +35,30 @@ public class DuckMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = GetPosition((Time.time + StartOffset) * RotationTime * 360);
+        transform.localPosition = GetSwimPosition((Time.time + StartOffset) * RotationTime * 360);
+        Model.transform.localEulerAngles = GetSwimRotation((Time.time + StartOffset) * BobblingSpeed);
     }
 
-    Vector3 GetPosition(float Time){
-        Vector3 pos = new Vector3(0,CentrePiscine.position.y,DistanceFromCenter);
+    Vector3 GetSwimPosition(float Time){
+        Vector3 pos = new Vector3(0,CentrePiscine.localPosition.y,DistanceFromCenter);
         
         float distanceNoise = Mathf.PerlinNoise(Time * DistanceNoiseScale,0);
         distanceNoise = distanceNoise - 0.5f;
         distanceNoise = distanceNoise * DistanceNoiseStrength;
         pos.z += distanceNoise;
 
-        Vector3 finalpos = RotatePointAroundPivot(pos, CentrePiscine.position, new Vector3(0,Time,0));
+        Vector3 finalpos = RotatePointAroundPivot(pos, CentrePiscine.localPosition, new Vector3(0,Time,0));
+        finalpos.y += (Mathf.Sin((Time / 360) * 16) * FloatFactor) / 2;
 
         return finalpos;
+    }
+
+    Vector3 GetSwimRotation(float Time){
+        Vector3 rotation = Vector3.zero;
+        rotation.x = Mathf.Cos(Time);
+        rotation.z = Mathf.Sin(Time);
+        rotation = rotation * BobbleAngle;
+        return rotation;
     }
 
     public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
