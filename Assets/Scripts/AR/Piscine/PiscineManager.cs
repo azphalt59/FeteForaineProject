dc614c4bbf3a6eb3f2244ef8950d452071cd5465
PaperMartin <martin.wetischek@orange.fr>
2021-04-03 16:03:24 +0200

    TexteIndice

--- Assets/Scripts/AR/Piscine/PiscineManager.cs
+++ Assets/Scripts/AR/Piscine/PiscineManager.cs
@@ -10,6 +10,14 @@ public class PiscineManager : MonoBehaviour
     private List<Text> LettersGO = new List<Text>();
     [SerializeField]
     private List<string> Letters;
+    [SerializeField]
+    private Text Indice;
+
+    [SerializeField]
+    private string IndiceSiMauvaixOrdre;
+    
+    [SerializeField]
+    private string IndiceSiBonOrdre;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +27,29 @@ public class PiscineManager : MonoBehaviour
             letter.gameObject.SetActive(false);
             letter.text = Letters[LettersGO.IndexOf(letter)];
         }
+        string totalLetters = null;
+            foreach(string Letter in Letters){
+                totalLetters += Letter;
+            }
+        if (totalLetters == "SECRET"){
+            Indice.text = totalLetters + IndiceSiBonOrdre;
+        }
+        else {
+            Indice.text = totalLetters + IndiceSiMauvaixOrdre;
+        }
     }
 
     public void ShowLetter(int index){
         LettersGO[index].gameObject.SetActive(true);
+        bool AllLettersVisible = true;
+        foreach (Text Letter in LettersGO)
+        {
+            if (!Letter.gameObject.activeSelf){
+                AllLettersVisible = false;
+                break;
+            }
+        }
+        Indice.gameObject.SetActive(AllLettersVisible);
     }
 
 }
